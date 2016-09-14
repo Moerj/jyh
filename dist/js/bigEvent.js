c14319c0d3bb95a5907ed4beb40d7a69aa4a9355
Moer <199525807@qq.com>
2016-09-14 16:12:00 +0800

    删除多余js

--- dist/js/bigEvent.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-(function () {
-    var timeline = document.getElementsByClassName('timeline');
-    for (var i = 1; i < timeline.length; i++) {
-        timeline[i].setAttribute('data-wow-delay', i * 0.3 + 's');
-    }
-})();
-//# sourceMappingURL=bigEvent.js.map
--- src/js/bigEvent.js
+++
@@ -1,6 +0,0 @@
-(function(){
-    var timeline = document.getElementsByClassName('timeline');
-    for (var i = 1; i < timeline.length; i++) {
-        timeline[i].setAttribute('data-wow-delay', i*0.3 + 's');
-    }
-})()
